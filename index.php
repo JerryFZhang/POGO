2f271474afb1e397043f9c79815fb14701ff8ddc
Fengwei Jerry Zhang <zhang96@users.noreply.github.com>
2017-10-23 22:08:09 -0400

    php update

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    // Initialize the language code variable
+$lc = "";
+    // Check to see that the global language server variable isset()
+    // If it is set, we cut the first two characters from that string
+if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+    $lc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    // Now we simply evaluate that variable to detect specific languages
+if($lc == "zh"){
+    include_once("home-zh.html");
+    exit();
+} else if($lc == "fr"){
+    include_once("home-en.html");
+    //will do later
+    exit();
+}
+else{ // don't forget the default case if $lc is empty
+    include_once("home-en.html");
+    exit();
+}
+?>
